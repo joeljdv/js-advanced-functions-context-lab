faa387b0a6c6668e66782a395eb8af8b9a7c0ec1
joeljdv <joeldiazjdv@gmail.com>
2021-04-04 23:28:13 -0400

    commit tree 911a7741aeaeefa4af92ef23746be8187dcd551c

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 /* Your Code Here */
+function createEmployeeRecord([string, stringTwo,stringThree, num]) {
+    const employee = {
+        firstName: string, familyName: stringTwo, title: stringThree, payPerHour: num,
+        timeInEvents:[] , timeOutEvents:[]
+    }
+    return employee
+}
+function createEmployeeRecords(arrayOne) {
+    return arrayOne.map(data =>{
+        return createEmployeeRecord(data)
+       })
+}
 
+function createTimeInEvent(dateStamp) {
+    let stampSplit = dateStamp.split(" ")
+    console.log(`stampSplit is: ${stampSplit}`);   
+    let hour = stampSplit[1];
+    let date = stampSplit[0]
+    this.timeInEvents.push({
+        type:"TimeIn",
+        hour: parseInt(hour,10),
+        date: date
+    })
+}
+ 
+
+
+
+
+
+// function hoursWorkedOnDate(date) {
+//     const timeInArray = this.timeInEvents
+//     const timeOutArray = this.timeOutEvents
+//     const timeInObject = timeInArray.find(e => e.date === date)
+//     const timeOutObject = timeOutArray.find(e => e.date === date)
+
+//     return(timeOutObject.hour - timeInObject.hour) / 100
+// }
 /*
+
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
